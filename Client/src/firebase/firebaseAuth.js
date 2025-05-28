39c2fd16663c50f1898571030e4ec6ece11aa8de
sanaeouj <sanae.ouajhain@gmail.com>
2025-05-28 09:01:07 +0100

    commit

--- Client/src/firebase/firebaseAuth.js
+++ Client/src/firebase/firebaseAuth.js
@@ -1,11 +1,12 @@
 import { getAuth, GoogleAuthProvider } from "firebase/auth";
+import { getFirestore } from "firebase/firestore"; 
 import app from "./firebase.js";
 
 const auth = getAuth(app);
-
 const googleProvider = new GoogleAuthProvider();
+const db = getFirestore(app);  
 googleProvider.setCustomParameters({
   prompt: "select_account",
 });
 
-export { auth, googleProvider };
+export { auth, googleProvider, db }; 
